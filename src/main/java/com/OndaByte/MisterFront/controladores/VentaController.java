bb4eb53a3e0d005df6314742253220b98e6a2ef2
LMGurruchaga <lmgurruchaga@cat-technologies.com>
2025-08-20 10:28:32 -0300

    Estructura Paneles venta

---
+++ src/main/java/com/OndaByte/MisterFront/controladores/VentaController.java
@@ -0,0 +1,223 @@
+
+package com.OndaByte.MisterFront.controladores;
+
+import com.OndaByte.MisterFront.modelos.Cliente;
+import com.OndaByte.MisterFront.modelos.ItemVenta;
+import com.OndaByte.MisterFront.modelos.Orden;
+import com.OndaByte.MisterFront.modelos.Venta;
+import com.OndaByte.MisterFront.servicios.VentaService;
+import com.OndaByte.MisterFront.sesion.SesionController;
+import com.OndaByte.MisterFront.vistas.DatosListener;
+import com.OndaByte.MisterFront.vistas.util.Paginado;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class VentaController {
+    
+    private SesionController sesionController = null;
+    private static VentaController instance;
+    private static Logger logger = LogManager.getLogger(VentaController.class.getName());
+       
+    static{
+        if(logger.isDebugEnabled()){
+            logger.debug("Init logger en VentaController");
+        }
+    }
+
+    private VentaController() {
+        this.sesionController = SesionController.getInstance();
+    }
+
+    public static VentaController getInstance() {
+        if (instance == null) {
+            instance = new VentaController();
+        }
+        return instance;
+    }
+
+    public void filtrar(String filtro, String desde, String hasta, String estado, String pagina, String cantElementos, DatosListener<List<HashMap<String, Object>>> listener) {
+        JSONObject ventasRes = VentaService.filtrar(filtro,desde,hasta, estado, pagina, cantElementos);
+        if (ventasRes.getInt("status") == 200) {
+            try {
+                JSONArray ventasArray = new JSONArray(ventasRes.getString("data"));
+                List<HashMap<String, Object>> ventasDTO = new ArrayList<>();
+
+                for (int i = 0; i < ventasArray.length(); i++) {
+
+                    JSONObject rJson = ventasArray.getJSONObject(i).getJSONObject("venta");
+                    JSONObject cJson = ventasArray.getJSONObject(i).getJSONObject("cliente");
+                    JSONObject oJson = ventasArray.getJSONObject(i).getJSONObject("orden");
+//                  JSONObject preJson = ventasArray.getJSONObject(i).getJSONObject("presupuesto");
+
+                    Venta r = new Venta();
+                    r.setId(rJson.getInt("id"));
+                    r.setFecha_emision(rJson.getString("fecha_emision"));
+                    r.setFecha_pago(rJson.optString("fecha_pago",null));
+                    r.setTotal(rJson.getFloat("total"));
+                    r.setObservaciones(rJson.optString("observaciones",null));
+                    r.setC_cuit_cuil(rJson.getString("cliente_cuit_cuil"));
+                    r.setC_domicilio(rJson.getString("cliente_domicilio"));
+                    r.setC_localidad(rJson.getString("cliente_localidad"));
+                    r.setC_nombre(rJson.getString("cliente_nombre"));
+                    r.setC_telefono(rJson.getString("cliente_telefono"));
+                    r.setPunto_venta(rJson.getString("punto_venta"));
+                    r.setNro_venta(rJson.getInt("nro_venta"));
+
+
+                    Cliente c = new Cliente();
+                    c.setId(cJson.getInt("id"));
+                    c.setNombre(cJson.getString("nombre"));
+                    c.setTelefono(cJson.getString("telefono"));
+                    
+//                    Presupuesto pre = new Presupuesto();
+//                    if(!preJson.isNull("id")){
+//                        pre.setId(preJson.optInt("id"));
+//                        pre.setNombre(preJson.getString("nombre"));
+//                        pre.setDescripcion(preJson.getString("descripcion"));
+//                        pre.setEstado_presupuesto(preJson.getString("estado_presupuesto"));
+//
+//                    }
+                    
+                    Orden o = new Orden();
+                    o.setId(oJson.getInt("id"));
+                    o.setDescripcion(oJson.optString("descripcion",null));
+                    o.setFecha_fin(oJson.optString("fecha_fin",null));
+                    o.setTipo(oJson.getString("tipo"));
+                    o.setEstado_orden(oJson.getString("estado_orden"));
+                    o.setCreado(oJson.getString("creado"));
+
+                    HashMap<String, Object> objeto = new HashMap<>();
+                    objeto.put("orden", o);
+                    objeto.put("cliente", c);
+                 //   objeto.put("turno", t);
+                    objeto.put("venta", r);
+                    ventasDTO.add(objeto);
+                }
+
+                Paginado p = new Paginado();
+                p.setPagina(ventasRes.getInt("pagina"));
+                p.setTamPagina(ventasRes.getInt("elementos"));
+                p.setTotalElementos(ventasRes.getInt("t_elementos"));
+                p.setTotalPaginas(ventasRes.getInt("t_paginas"));
+
+                listener.onSuccess(ventasDTO, p);
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.debug(e.getMessage());
+                listener.onError("Error al procesar ventas");
+            }
+        } else {
+            listener.onError(ventasRes.optString("mensaje"));
+        }
+    }
+    
+    public void buscarVenta(int id, DatosListener<HashMap<String, Object>> listener) { 
+        JSONObject res = VentaService.buscarVenta(id);
+        if (res.getInt("status") == 200) {
+            try {
+                JSONObject rDetalle = new JSONObject(res.getString("data"));
+                HashMap<String, Object> ventaDTO = new HashMap<>();
+                ArrayList<ItemVenta> itemsR = new ArrayList<>();
+                
+                JSONObject rJson = rDetalle.getJSONObject("venta");
+                JSONObject oJson = rDetalle.getJSONObject("orden");
+                JSONObject cJson = rDetalle.getJSONObject("cliente");
+                JSONArray itemsRJson = rDetalle.getJSONArray("items");
+
+                Venta r = new Venta();
+                r.setId(rJson.getInt("id"));
+                r.setFecha_emision(rJson.getString("fecha_emision"));
+                r.setFecha_pago(rJson.optString("fecha_pago",null));
+                r.setTotal(rJson.getFloat("total"));
+                r.setObservaciones(rJson.optString("observaciones",null));
+                    
+                Cliente c = new Cliente();
+                c.setId(cJson.getInt("id"));
+                c.setNombre(cJson.getString("nombre"));
+                c.setTelefono(cJson.getString("telefono"));
+
+//                Presupuesto pre = new Presupuesto();
+//                pre.setId(preJson.getInt("id"));
+//                pre.setNombre(preJson.getString("nombre"));
+//                pre.setDescripcion(preJson.getString("descripcion"));
+//                pre.setEstado_presupuesto(preJson.getString("estado_presupuesto"));
+//                pre.setTotal(preJson.getFloat("total"));
+
+                for (int i = 0; i < itemsRJson.length(); i++) {
+                    ItemVenta ir = new ItemVenta();
+                    ir.setId(itemsRJson.getJSONObject(i).getInt("id"));
+                    ir.setDescripcion(itemsRJson.getJSONObject(i).getString("descripcion"));
+                    ir.setCantidad(itemsRJson.getJSONObject(i).optIntegerObject("cantidad",null));
+                    ir.setPrecio(itemsRJson.getJSONObject(i).getFloat("precio"));
+                    itemsR.add(ir);
+                }
+//                presupuestoDTO.put("pedido", p);
+                ventaDTO.put("venta", r);
+                ventaDTO.put("cliente", c);
+                ventaDTO.put("items", itemsR); 
+
+                listener.onSuccess(ventaDTO);
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.debug(e.getMessage());
+                listener.onError("Error al procesar ventas");
+            }
+        } else {
+            listener.onError(res.optString("mensaje"));
+        }
+    }
+    
+//    /**
+//     *
+//     */
+//    public void actualizarVenta(Venta r, DatosListener<String> listener) { 
+//        JSONObject res = VentaService.actualizarVenta(r);
+//        if (res.getInt("status") == 201) {
+//            listener.onSuccess(res.optString("mensaje"));
+//        } else {
+//            listener.onError(res.optString("mensaje"));
+//        }
+//    }
+     
+    /**
+     * Crea un nuevo venta.
+     */
+    public void crearVenta(Venta venta,List<ItemVenta> items, DatosListener<String> listener) {
+        JSONObject res = VentaService.crearVenta(venta,items);
+        if (res.getInt("status") == 201) {
+            listener.onSuccess(res.optString("mensaje"));
+        } else {    
+            listener.onError(res.optString("mensaje"));
+        }
+    }
+
+    /**
+     * Crea un nuevo venta.
+     */
+    public void editarVenta(Venta venta, List<ItemVenta> items, DatosListener<String> listener) {
+        JSONObject res = VentaService.editarVenta(venta,items);
+        if (res.getInt("status") == 201) {
+            listener.onSuccess(res.optString("mensaje"));
+        } else {    
+            listener.onError(res.optString("mensaje"));
+        }
+    }
+
+    /**
+     * Elimina un venta por ID.
+     */
+    public void eliminarVenta(int id, DatosListener<String> listener) {
+        JSONObject res = VentaService.eliminarVenta(id);
+        if (res.getInt("status") == 200) {
+            listener.onSuccess(res.optString("mensaje"));
+        } else {
+            listener.onError(res.optString("mensaje"));
+        }
+    }
+}
---
+++ src/main/java/com/OndaByte/MisterFront/servicios/VentaService.java
@@ -0,0 +1,100 @@
+
+package com.OndaByte.MisterFront.servicios;
+
+import com.OndaByte.MisterFront.modelos.ItemVenta;
+import com.OndaByte.MisterFront.modelos.Venta;
+import static com.OndaByte.MisterFront.servicios.APIRequest.enviarRequest;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class VentaService {
+    private static Logger logger = LogManager.getLogger(VentaService.class.getName());
+
+    static {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Init logger en VentaService");
+        }
+    }
+
+    /**
+     * Obtiene la lista de Ventas desde el API.
+     */
+    public static JSONObject getVentas() {
+        return enviarRequest("/p/e/ventas", "GET", null);
+    }
+
+    /**
+     * Crea un Venta en la API.
+     */
+    public static JSONObject crearVenta(Venta venta, List<ItemVenta> items) {
+        //hacer json 
+        JSONObject jo = new JSONObject();
+        JSONObject pre = new JSONObject(venta);
+        JSONArray jaItems= new JSONArray();
+        for(ItemVenta i : items){
+            jaItems.put(new JSONObject(i));
+        }
+        jo.put("venta",pre);
+        jo.put("items",jaItems);
+        return enviarRequest("/p/e/venta", "POST", jo);
+    }
+
+    /**
+     * Crea un Venta en la API.
+     */
+    public static JSONObject editarVenta(Venta venta, List<ItemVenta> items) {
+        //hacer json 
+        JSONObject jo = new JSONObject();
+        JSONObject r = new JSONObject(venta);
+        JSONArray jaItems= new JSONArray();
+        for(ItemVenta i : items){
+            jaItems.put(new JSONObject(i));
+        }
+        jo.put("venta",r);
+        jo.put("items",jaItems);
+        return enviarRequest("/p/e/venta/"+venta.getId(), "PUT", jo);
+    }
+    /**
+     * Elimina un Venta por ID en la API.
+     */
+    public static JSONObject eliminarVenta(int id) {
+        return enviarRequest("/p/a/venta/" + id + "/baja", "DELETE", null);
+    }
+
+    public static JSONObject filtrar(String filtro, String desde, String hasta, String estado, String pagina, String cantElementos) {
+        try {
+            String filtroCodificado = URLEncoder.encode(filtro, StandardCharsets.UTF_8.toString());
+            String desdeCodificado = URLEncoder.encode(desde, StandardCharsets.UTF_8.toString());
+            String hastaCodificado = URLEncoder.encode(hasta, StandardCharsets.UTF_8.toString());
+            String estadoCodificado = URLEncoder.encode(estado, StandardCharsets.UTF_8.toString());
+            return enviarRequest(
+                "/p/e/venta?filtro=" + filtroCodificado + "&desde=" + desdeCodificado + "&hasta=" + hastaCodificado + "&estado=" + estadoCodificado + "&pagina=" + pagina + "&elementos=" + cantElementos,
+                "GET",
+                null
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new JSONObject()
+                .put("status", 500)
+                .put("mensaje", "Error al codificar el filtro");
+        }
+    }
+    
+//    /**
+//     * Crea un Pedido en la API.
+//     */
+//    public static JSONObject actualizarVenta(Venta v) {
+//        JSONObject jo = new JSONObject();
+//        jo.put("fecha_pago",r.getFecha_pago());
+//        return enviarRequest("/p/e/venta/"+v.getId()+"/actualizar", "PUT", jo);
+//    }
+
+    public static JSONObject buscarVenta(int id) {
+        return enviarRequest("/p/e/venta/" +id, "get", null);
+    }
+}
